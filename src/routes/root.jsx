ddf54674f9241e730afaa1df39db31afd4fcc9b5
adriabama06 <adriabama06@gmail.com>
2023-12-14 22:13:21 +0100

    Add CSS

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -53,7 +53,8 @@ function App() {
           });
           */
          updateCounter();
-        }}>Clieck Me!</button>
+        }}
+        style={{cursor: "pointer"}}>Clieck Me!</button>
       </div>
     </div>
   )
